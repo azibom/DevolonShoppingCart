421f25647aa1f3f4077844393ff6fc1ca5fcdbc6
azibom <azibom@azibom.com>
2021-05-07 14:21:57 +0430

    change the main.php and update it

--- main.php
+++ main.php
@@ -1,93 +1,14 @@
 <?php
+require './vendor/autoload.php';
 
-class Supermarket {
-    private $order;
-    private $products = [];
+use Devolon\ShoppingCart\Input\CliInput;
+use Devolon\ShoppingCart\Supermarket\Supermarket;
 
-    public function setProducts(array $products)
-    {
-        foreach ($products as $product) {
-            if (count($product) > 2) {
-                $productObject = new ProductWithSpecialPrice($product[0], $product[1]);
-                for ($i=2; $i < count($product); $i++) { 
-                    $specialOffer = explode('-', $product[$i]);
-                    $productObject->setSpecialPrice((int)$specialOffer[0], (int)$specialOffer[1]);
-                }
-
-                $this->products[$product[0]] = $productObject;
-            } else {
-                $this->products[$product[0]] = new Product($product[0], $product[1]);
-            }
-
-        }
-    }
-
-    public function setOrder(array $productNames)
-    {
-        $this->order = array_count_values($productNames);
-    }
-
-    public function calculateTotalPrice()
-    {
-        $totalPrice = 0;
-        foreach ($this->order as $productName => $productCount) {
-            $totalPrice += $this->products[$productName]->calculatePrice($productCount);
-        }
-
-        return $totalPrice;
-    }
-}
-
-class Product {
-    protected $name;
-    protected $price;
-    public function __construct($name, $price)
-    {
-        $this->name = $name;
-        $this->price = $price;
-    }
-
-    public function calculatePrice(int $productCount)
-    {
-        return $productCount * $this->price;
-    }
-}
-
-
-class ProductWithSpecialPrice extends Product {
-    private $specialPrices = [];
-
-    public function setSpecialPrice($count, $price)
-    {
-        $this->specialPrices[$count] = $price;
-    }
-
-    public function calculatePrice(int $count)
-    {
-        $price = 0;
-        krsort($this->specialPrices);
-        foreach ($this->specialPrices as $productCount => $productPrice) {
-            if ($count >= $productCount) {
-                $price += ((int)($count / $productCount) * $productPrice);
-                $count = $count % $productCount;
-            }
-
-            $price += $count * $this->price;
-        }
-
-        return $price;
-    }
-}
-
-$count = (int)fgets(STDIN);
-$productsData = [];
-for ($i=0; $i < $count; $i++) { 
-    $productsData[] = explode(' ', str_replace(PHP_EOL, '', fgets(STDIN)));
-}
-
-$input = explode(' ', str_replace(PHP_EOL, '', fgets(STDIN)));
-
+$inputer = new CliInput;
+$productsData = $inputer->getProducts();
+$order        = $inputer->getOrder();
 $supermarket = new Supermarket;
 $supermarket->setProducts($productsData);
-$supermarket->setOrder($input);
+$supermarket->setOrder($order);
+
 echo $supermarket->calculateTotalPrice() . PHP_EOL;
