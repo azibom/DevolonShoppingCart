37cce45b236b80aece615ef9a46aff46f30f0027
azibom <azibom@azibom.com>
2021-05-07 17:12:23 +0430

    add const to the ProductFactory and check the specialPrices too

--- src/Factories/ProductFactory.php
+++ src/Factories/ProductFactory.php
@@ -7,21 +7,26 @@ use Devolon\ShoppingCart\Product\Product;
 use Devolon\ShoppingCart\Product\ProductWithSpecialPrice;
 
 class ProductFactory {
+    const PRODUCT = "product";
+    const PRODUCT_WITH_SPECIAL_PRICE = "productWithSpecialPrice";
+
     public function getInstance(string $productName, $data) {
         switch ($productName) {
-            case "product":
+            case self::PRODUCT:
                 $product = new Product($data['name'], $data['price']);
             break;
-            case "productWithSpecialPrice":
+            case self::PRODUCT_WITH_SPECIAL_PRICE:
                 $product = new ProductWithSpecialPrice($data['name'], $data['price']);
 
-                foreach ($data['specialPrices'] as $specialPrice) {
-                    $specialPrice = explode('-', $specialPrice);
-                    $product->setSpecialPrice((int)$specialPrice[0], (int)$specialPrice[1]);
+                if (isset($data['specialPrices'])) {
+                    foreach ($data['specialPrices'] as $specialPrice) {
+                        $specialPrice = explode('-', $specialPrice);
+                        $product->setSpecialPrice((int)$specialPrice[0], (int)$specialPrice[1]);
+                    }
                 }
             break;
             default:
-                throw new Exception("$productName product Type Not Found.");
+                throw new Exception("$productName Product Type Not Found.");
             break;    
         }
 
